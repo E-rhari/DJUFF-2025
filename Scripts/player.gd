2f2c0e1c4376d478577a848c9b36d9b9f3bfdcd5
E-Rhari <93932260+E-rhari@users.noreply.github.com>
2025-06-18 11:34:26 -0300

    Movimento Horizontal com Flip de CharacterBody2D

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,10 +2,19 @@ extends CharacterBody2D
 
 @export var speed = 400
 
-func get_input():
-	var input_direction = Input.get_axis("left", "right")
-	velocity = input_direction*transform.x*speed
+
+func directional_flip(direction):
+	if direction < 0:
+		transform.x.x = -1;
+	else:
+		transform.x.x = 1
+
 
 func _physics_process(delta: float) -> void:
-	get_input()
+	var input_direction = Input.get_axis("left", "right")
+	if input_direction:
+		velocity.x = input_direction*speed
+		directional_flip(input_direction)
+	else:
+		velocity.x = input_direction*0
 	move_and_slide()

a2f0740eefe35de94c312e8008597468dae33ece
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2025-06-18 20:31:14 -0300

    Pulo v1.0
    O pulo agora é regulável, ou seja, o tamanho do pulo é relativo a quanto tempo o jogador esteve apertando o botão.
    Enquanto o jogador aperta o botão, o personagem está se movendo para cima (pulando). Quando ele solta, o personagem para de subir.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,16 +13,36 @@ func directional_flip(direction):
 		transform.x.x = 1
 
 
-func _physics_process(delta: float) -> void:
-	#get_input()
-	velocity.y += gravity * delta
-	
-	if (Input.is_action_just_pressed("up") and is_on_floor()):
-		velocity.y = -jump_speed
+func jump():
+	velocity.y = -jump_speed
+
+
+func horizontal_movement():
 	var input_direction = Input.get_axis("left", "right")
 	if input_direction:
 		velocity.x = input_direction*speed
 		directional_flip(input_direction)
 	else:
 		velocity.x = input_direction*0
+
+
+func apply_gravity(delta: float):
+	velocity.y += gravity * delta
+
+
+func cut_jump():
+	if velocity.y < float(-speed)/4:
+		velocity.y = float(-speed)/4
+
+
+func _physics_process(delta: float) -> void:
+	#get_input()
+	apply_gravity(delta)
+	horizontal_movement()
+	
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		jump()
+	if Input.is_action_just_released("up"):
+		cut_jump()
+	
 	move_and_slide()

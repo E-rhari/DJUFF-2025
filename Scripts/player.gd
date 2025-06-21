d38f648915be3aea10bf5b186e9d1606abebaf1e
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-06-21 19:46:52 -0300

    Som do pulo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -55,6 +55,7 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		jump()
+		$SomDoPulo.play()
 	if Input.is_action_just_released("up"):
 		cut_jump()
 	
